797e5a2c2a3febd4769de6d80955b6dcaeb06f75
TanninOne <seppleviathan@gmx.de>
2018-10-10 10:09:21 +0200

    added dashboard button

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import { selectImportFolder, setImportStep } from './actions/session';
+import { setImportStep } from './actions/session';
 import { sessionReducer } from './reducers/session';
 import ImportDialog from './views/ImportDialog';
 
-import * as Promise from 'bluebird';
+import { app as appIn, remote } from 'electron';
 import * as path from 'path';
-import { fs, selectors, types, util } from 'vortex-api';
+import { fs, types } from 'vortex-api';
+
+const app = appIn !== undefined ? appIn : remote.app;
+
+function nmmConfigExists(): boolean {
+  try {
+    const base = path.resolve(app.getPath('appData'), '..', 'local', 'Black_Tree_Gaming');
+    fs.statSync(base);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
 
 function init(context: types.IExtensionContext): boolean {
   if (process.platform !== 'win32') {
@@ -19,6 +31,10 @@ function init(context: types.IExtensionContext): boolean {
   context.registerAction('mod-icons', 115, 'import', {}, 'Import From NMM', () => {
     context.api.store.dispatch(setImportStep('start'));
   });
+
+  context.registerToDo('import-nmm', 'search', () => ({}), 'import', 'Import from NMM', () => {
+    context.api.store.dispatch(setImportStep('start'));
+  }, () => nmmConfigExists(),  '', 100);
 
   context.once(() => {
     context.api.setStylesheet('nmm-import-tool', path.join(__dirname, 'import-tool.scss'));
