89c1b8936cc16af04440ed73aa681b41c2a9cf01
IDCs <idc.dts@gmail.com>
2019-07-01 09:35:33 +0100

    Do not calculate disk space if modsToImport length is 0

--- src/views/ImportDialog.tsx
+++ src/views/ImportDialog.tsx
@@ -359,7 +359,7 @@ class ImportDialog extends ComponentEx<IProps, IComponentState> {
 
   private onStartUp(): Promise<void> {
     const { modsToImport } = this.state;
-    if (modsToImport === undefined) {
+    if ((modsToImport === undefined) || (Object.keys(modsToImport).length === 0)) {
       // happens if there are no NMM mods for this game
       return Promise.resolve();
     }
