2e913cbe52e905c4ac2dfeb2396a513ac3ef5c05
IDCs <idc.dts@gmail.com>
2019-07-12 14:16:20 +0100

    fix: ignore non-existant archives - NMM deployment manifest may contain information about mods/archives   which no longer exist. This would cause the import process to wrongly   assume that the NMM still has deployed files.

--- src/util/nmmVirtualConfigParser.ts
+++ src/util/nmmVirtualConfigParser.ts
@@ -14,26 +14,8 @@ export function parseNMMConfigFile(nmmFilePath: string, mods: IModMap): Promise<
       .catch(
           err => Promise.reject(new ParseError(
               'The selected folder does not contain a VirtualModConfig.xml file.')))
-      .then(data => parseModEntries(data.toString('utf-8'), mods, path.dirname(nmmFilePath)));
-}
-
-function getInner(ele: Element, tagName: string): string {
-  return ele.getElementsByTagName(tagName)[0].childNodes[0].nodeValue;
-}
-
-function transformFileEntry(
-    fileLink: Element, adjustRealPath: (input: string) => string): IFileEntry {
-  const virtualPath = fileLink.getAttribute('virtualPath');
-  if ((virtualPath === null) || (virtualPath === undefined)) {
-    return undefined;
-  }
-
-  return {
-    fileSource: adjustRealPath(fileLink.getAttribute('realPath')),
-    fileDestination: virtualPath,
-    isActive: getInner(fileLink, 'isActive').toLowerCase() === 'true',
-    filePriority: parseInt(getInner(fileLink, 'linkPriority'), 10),
-  };
+      .then(data => parseModEntries(data.toString('utf-8'), mods, path.dirname(nmmFilePath))
+        .filter(entry => entry !== undefined));
 }
 
 // exported so it can be unit-tested (ugh)
@@ -85,49 +67,16 @@ export function parseModEntries(
     res.vortexId = util.deriveInstallName(archiveName, {});
     res.isAlreadyManaged = modListSet.has(res.vortexId);
 
-    let useOldPath: boolean = false;
-
-    // NMM has a crazy workaround where it will use an install path based on
-    // the download id or the archive name, whatever is available.
-    // Manually added mods seem to have neither! attempt to use the mod's name
-    //  as a final resort.
-    const adjustRealPath = (input: string): string => {
-      return path.join(useOldPath
-        ? archiveName
-        : (res.downloadId !== undefined)
-          ? res.downloadId.toString()
-          : res.modName,
-        ...input.split(path.sep).slice(1));
-    };
-
     return fs.statAsync(path.join(virtualInstallPath, archiveName))
-        .then(() => { useOldPath = true; })
-        .catch(() => undefined)
-        .then(() => {
-          let fileLinks: NodeListOf<Element>;
-          try {
-            fileLinks = modInfo.getElementsByTagName('fileLink') as any;
-          } catch (err) {
-            log('warn', 'VirtualModConfig.xml has no file links for mod',
-                { archiveName, error: err.message });
-          }
-
-          // if ((fileLinks !== undefined) && (fileLinks.length > 0)) {
-          //   res.fileEntries =
-          //       Array.from(fileLinks)
-          //           .map(fileLink => transformFileEntry(fileLink, adjustRealPath))
-          //           .filter(entry => entry !== undefined);
-          // }
-
-          const modFilePath = path.join(res.archivePath, res.modFilename);
-
-          return modmeta.genHash(modFilePath);
-        })
-        .then((hashResult: modmeta.IHashResult) => {
-          res.archiveMD5 = hashResult.md5sum;
-        })
-        .catch(() => { res.archiveMD5 = null; })
-        .then(() => res);
+      .then(() => {
+        const modFilePath = path.join(res.archivePath, res.modFilename);
+        return modmeta.genHash(modFilePath);
+      })
+      .then((hashResult: modmeta.IHashResult) => {
+        res.archiveMD5 = hashResult.md5sum;
+      })
+      .then(() => res)
+      .catch(() => undefined);
   });
 }
 
